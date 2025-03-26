9f1881f92c1c9bd50a8f92ddbb76a2476594ac6e
Thavasumoorthi <thavasumoorthi@gmail.com>
2025-03-26 16:22:40 +0530

    modified another changes in the file

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
 print("Hello Python I am running from jenkins")
-print("Hello Python I am running from ecs ")
+print("Hello Python I am running from ecs container ")
+print("Hello Python I am running from ecs container2 ")
